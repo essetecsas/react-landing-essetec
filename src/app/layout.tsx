574be8abe749bfd69f73e6292a355550b78f5375
santiagoMoreno73 <santimor2009@hotmail.com>
2024-10-16 18:09:06 -0500

    change head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,10 +5,17 @@ import "bootstrap/dist/css/bootstrap.css";
 import BootstrapClient from "./components/BootstrapClient/BootstrapClient";
 import { BaseLayout } from "./components/layouts";
 import { FloatButton } from "./components/ui";
+import Head from "next/head";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="eLtZVRqL1C2l1RBNKajVytGyQPAGF0pfrW6VWG-w9-A"
+        />
+      </Head>
       <body>
         <BaseLayout>{children}</BaseLayout>
         <BootstrapClient />
