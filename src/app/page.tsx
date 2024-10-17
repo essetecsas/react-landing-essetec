ac36e5c2c712917eca0c0f000db1e041d00783b0
santiagoMoreno73 <santimor2009@hotmail.com>
2024-10-17 09:15:46 -0500

    added metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,27 @@ import {
 import { HeroSection } from "./components/ui";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://www.essetecsas.com/"),
   title: "Essetec | Inicio",
   description:
     "Soluciones integrales que garanticen la protección de propiedades y activos mediante el uso de tecnologías innovadoras.",
+  keywords: [
+    "essetec",
+    "tecnología",
+    "programación",
+    "redes",
+    "infraestructura",
+    "seguridad",
+    "monitoreo",
+    "vigilancia",
+    "escalabilidad",
+    "calidad",
+    "marketing",
+  ],
+  openGraph: {
+    description:
+      "Convierte tu negocio en una experiencia tecnológica sorprendente, única e innovadora",
+  },
   other: {
     "google-site-verification": "eLtZVRqL1C2l1RBNKajVytGyQPAGF0pfrW6VWG-w9-A",
   },
